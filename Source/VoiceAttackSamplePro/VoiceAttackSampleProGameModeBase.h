3f53f9d02ffb5eaf40e37c1c54f9a61e099fa7a5
Corneel <corneelbooysen@hotmail.com>
2019-05-01 20:01:11 -0500

    New branch

---
+++ Source/VoiceAttackSamplePro/VoiceAttackSampleProGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VoiceAttackSampleProGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOICEATTACKSAMPLEPRO_API AVoiceAttackSampleProGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
